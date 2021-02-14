ca9bf88054fd394accdc0d0587110f5c52f6a3c6
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-02-14 14:33:40 +0400

    Added zip exception for git files

--- scripts/server.sh
+++ scripts/server.sh
@@ -57,10 +57,10 @@ cd ".."
 
 echo -e "\n\nZipping build folder\n=============================================="
 if [[ $(uname) =~ "CYGWIN" || $(uname) =~ "MINGW" || $(uname) =~ "MSYS" ]]; then
-    "../bin/7z.exe" a "libDrive_Server_$commit_id.zip" "./backend/*"
+    "../bin/7z.exe" a "libDrive_Server_$commit_id.zip" "./backend/*" -xr\!.git/ -x\!.gitignore
 elif [[ $(uname) =~ "Linux" ]]; then
     cd "./backend"
-    zip -r "libDrive_Server_$commit_id.zip" "./"
+    zip -r "libDrive_Server_$commit_id.zip" "./" -x ./.git/* ./.gitignore
     mv "libDrive_Server_$commit_id.zip" ".."
     cd ".."
 else
